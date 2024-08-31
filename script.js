91cf517c2cde4ee1289e4e3959683730e2aed885
Benchuk Dima <dimitron400033@gmail.com>
2024-08-31 13:47:48 +0300

    Добавил попытки и возможность продолжить игру после победы или поражения

--- script.js
+++ script.js
@@ -4,25 +4,43 @@ const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-function numberCheck(rNum) {
+function numberCheck(rNum, attempts) {
     return function (pNum) {
+
         if (pNum === 0) {
             alert('Игра окончена');
+        } else if (attempts === 1) {
+            if (confirm('Попытки закончились. Хотели бы сыграть еще?')) {
+                rNum = Math.ceil(Math.random() * 100);
+                attempts = 10;
+                randomNumber(+prompt("Угадай число от 1 до 100"));
+            } else {
+                alert('Игра окончена');
+            }
         } else if (isNumber(pNum) === false) {
             alert('Введи число!');
             randomNumber(+prompt("Угадай число от 1 до 100"));
         } else if (pNum > rNum) {
-            alert('Загаданное число меньше')
+            attempts--;
+            alert('Загаданное число меньше, осталось попыток: ' + attempts);
             randomNumber(+prompt("Угадай число от 1 до 100"));
         } else if (pNum < rNum) {
-            alert('Загаданное число больше')
+            attempts--;
+            alert('Загаданное число больше, осталось попыток: ' + attempts);
             randomNumber(+prompt("Угадай число от 1 до 100"));
         } else if (pNum === rNum) {
-            alert('Поздравляю, Вы угадали!!!');
+            if (confirm('Поздравляю, Вы угадали!!!. Хотели бы сыграть еще?')) {
+                rNum = Math.ceil(Math.random() * 100);
+                attempts = 10;
+                randomNumber(+prompt("Угадай число от 1 до 100"));
+            } else {
+                alert('Игра окончена');
+            }
         }
+
     }
 }
 
-let randomNumber = numberCheck(Math.ceil(Math.random() * 100));
+let randomNumber = numberCheck(Math.ceil(Math.random() * 100), 10);
 
 randomNumber(+prompt("Угадай число от 1 до 100"));
